620f2d02cc9e7aad00082a69889788c7c1bdd23e
Kent <642238553@qq.com>
2016-04-23 20:38:38 +0800

    cgal

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@
 #include <iostream>
 #include <string>
 #include <assimp/Importer.hpp>
+#include <CGAL/Surface_mesh_default_triangulation_3.h>
+#include <CGAL/Complex_2_in_triangulation_3.h>
+#include <CGAL/make_surface_mesh.h>
+#include <CGAL/Implicit_surface_3.h>
 
 
 using namespace std;
@@ -58,7 +62,7 @@ void error_out(int err, const char* desc) {
 	cout << "Error: " << err << " || " << desc << endl;
 };
 
-int main(int argc, const char * argv[]) {
+int main2(int argc, const char * argv[]) {
 	int w = 1024;
 	int h = 768;
 
@@ -182,4 +186,34 @@ int main(int argc, const char * argv[]) {
 
 	new Importer;
 };
+
+// default triangulation for Surface_mesher
+typedef CGAL::Surface_mesh_default_triangulation_3 Tr;
+// c2t3
+typedef CGAL::Complex_2_in_triangulation_3<Tr> C2t3;
+typedef Tr::Geom_traits GT;
+typedef GT::Sphere_3 Sphere_3;
+typedef GT::Point_3 Point_3;
+typedef GT::FT FT;
+typedef FT (*Function)(Point_3);
+typedef CGAL::Implicit_surface_3<GT, Function> Surface_3;
+FT sphere_function (Point_3 p) {
+  const FT x2=p.x()*p.x(), y2=p.y()*p.y(), z2=p.z()*p.z();
+  return x2+y2+z2-1;
+}
+int main() {
+  Tr tr;            // 3D-Delaunay triangulation
+  C2t3 c2t3 (tr);   // 2D-complex in 3D-Delaunay triangulation
+  // defining the surface
+  Surface_3 surface(sphere_function,             // pointer to function
+                    Sphere_3(CGAL::ORIGIN, 2.)); // bounding sphere
+  // Note that "2." above is the *squared* radius of the bounding sphere!
+  // defining meshing criteria
+  CGAL::Surface_mesh_default_criteria_3<Tr> criteria(30.,  // angular bound
+                                                     0.1,  // radius bound
+                                                     0.1); // distance bound
+  // meshing surface
+  CGAL::make_surface_mesh(c2t3, surface, criteria, CGAL::Non_manifold_tag());
+  std::cout << "Final number of points: " << tr.number_of_vertices() << "\n";
+}
 
